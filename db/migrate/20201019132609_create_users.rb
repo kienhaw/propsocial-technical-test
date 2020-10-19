7d70b14842995017d96ded084918f752aba35ca4
KienHaw <kienhaw94@gmail.com>
2020-10-19 21:27:04 +0800

    Migration - create user table

---
+++ db/migrate/20201019132609_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.date :dob
+      t.string :phone_number
+      t.string :nric
+      t.string :ic_image
+      t.string :email
+      t.string :digest_password
+
+      t.timestamps
+    end
+  end
+end
