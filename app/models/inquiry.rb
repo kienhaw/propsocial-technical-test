29b5cd5be048873bc92e179020bccecd67b09f6c
KienHaw <kienhaw94@gmail.com>
2020-10-21 14:18:15 +0800

    refine notification content message

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -5,7 +5,7 @@ class Inquiry < ApplicationRecord
   after_create do
     ad.source.notifications.create!(
       title: 'New inquiry',
-      content: "#{source.first_name} #{source.last_name} has sent a new inquiry",
+      content: "#{source.first_name} #{source.last_name} has sent a new inquiry for #{ad.property_name}",
       inquiry: self
     )
   end
