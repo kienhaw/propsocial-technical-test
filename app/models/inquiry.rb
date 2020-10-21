f6c2fa250acf55afea5136392dcc5b956e4c7b18
KienHaw <kienhaw94@gmail.com>
2020-10-21 13:10:49 +0800

    Refine - create notification for ad's creator instead

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -3,7 +3,7 @@ class Inquiry < ApplicationRecord
   belongs_to :source, polymorphic: true
 
   after_create do
-    source.notifications.create!(
+    ad.source.notifications.create!(
       title: 'New inquiry',
       content: "#{source.first_name} #{source.last_name} has sent a new inquiry",
       inquiry: self
