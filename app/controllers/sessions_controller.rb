232cac24d17a6b29f0b2f706a4729030193da4ee
KienHaw <kienhaw94@gmail.com>
2020-10-21 13:57:19 +0800

    skip authorise for new, create and login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
+
+  skip_before_action :authorized, only: [:new, :create, :login]
+
   def new
   end
 
@@ -13,7 +16,7 @@ class SessionsController < ApplicationController
        session[:user_id] = @user.id if params[:type] == "user"
        # session[:agent_id] = @user.id if params[:type] == "agent"
        session[:login_type] = params[:type]
-       redirect_to '/ads-listing'
+       redirect_to '/ads'
     else
       @error = {message: "Failed to login, invalid credentials", type: "user"}
       respond_to do |format|
@@ -25,5 +28,10 @@ class SessionsController < ApplicationController
   end
 
   def welcome
+  end
+
+  def logout
+    reset_session
+    redirect_to '/login'
   end
 end
